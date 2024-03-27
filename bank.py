5f7e7cc26232799f3e82b0a8b32e20e43d8ba252
Shuhbam Bhardwaj <bhardwa7@sheridancollege.ca>
2024-03-26 21:36:12 -0400

    In bank module/class: a loop was created to open Savings and chequing account in openaccount method.

--- bank.py
+++ bank.py
@@ -1,3 +1,5 @@
+import savingsAccount
+import chequingAccount
 class Bank:
 
     def __init__(self,bankName):
@@ -10,8 +12,45 @@ class Bank:
     def setBankName(self,bankName):
         if not bankName.strip():
             raise Exception("Name cannot be blank")
-        self.bankName = bankName.strip()      
+        self.bankName = bankName.strip()  
+
+
+
     def openAccount(self):
-        pass
+        while True:
+            print("Welcome! Please select an option to continue")
+            print("1. Open Savings Account")
+            print("2. Open Chequing Account")
+            print("3. Exit")
+            choice = int(input("Enter the choice: "))
+            if choice ==1:
+                print("************WELCOME TO YOUR SAVINGS ACCOUNT************")
+                accountNumber = int(input("Please enter your AccountNumber: "))
+                accountHolderName = input("Please enter your name: ")
+                amount = int(input("Pleae enter the amuount: $"))
+                savingsAccountObj1= savingsAccount.SavingsAccount(accountNumber,accountHolderName,amount)
+                self.accounts.append(savingsAccountObj1)
+                print(savingsAccountObj1)
+                print(self.accounts)
+                break
+
+            elif choice ==2:
+                print("************WELCOME TO YOUR CHEQUING ACCOUNT************")
+                accountHolderName = input("Please enter your name: ")
+                accountNumber = int(input("Please enter your Account Number: "))
+                amount = int(input("Please enter the amount: $"))
+                chequingAccountObj1= chequingAccount.ChequingAccount(accountNumber,accountHolderName,amount)
+                self.accounts.append(chequingAccountObj1)
+                print(chequingAccountObj1)  
+                print(self.accounts)
+                return self.accounts
+
+            elif choice ==3:
+                print("exit")
+                return self.accounts
+
+
+            
+                    
     def searchAccount(self):
         pass
