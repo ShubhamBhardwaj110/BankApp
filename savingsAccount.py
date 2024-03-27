93089f173115910a5ab0c130d7a07a4777d9c1c4
Shuhbam Bhardwaj <bhardwa7@sheridancollege.ca>
2024-03-26 21:12:49 -0400

    In savingsAccount module/class: 1. Account module is inherited and called in savingsAccount using super() 2. Withdraw method is created to override the withdraw method of account class

--- savingsAccount.py
+++ savingsAccount.py
@@ -1,7 +1,17 @@
-class SavingsAccount:
-    def __init__(self):
-        
+import account
+
+class ChequingAccount(account.Account):
+    def __init__(self, accountNumber, accountHolderName, currentBalance):
+        super().__init__(accountNumber, accountHolderName, currentBalance)
+        self.overdraftlimit = -5000
 
 
-    def withdraw(self):
-        pass
+    def getoverdraftLimit(self):
+         return self.overdraftlimit
+    
+    def withdraw(self,amount):
+        if self.currentBalance - amount >= self.overdraftlimit:
+            self.currentBalance -= amount
+        else:
+            raise Exception("Please enter the valid amount")
+        return  self.currentBalance 
