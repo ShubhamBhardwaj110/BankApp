de99f4d4fe502b797e4a43508aae9ee3bbf21041
Shuhbam Bhardwaj <bhardwa7@sheridancollege.ca>
2024-03-26 21:43:24 -0400

    A loop is created in show main menu method of bankApplication class/module. This loop will ask user to explore account and create new account.

--- bankApplication.py
+++ bankApplication.py
@@ -1,12 +1,31 @@
+import bank
 class bankApplication:
     def __init__(self):
-        pass
+        self.bank_obj = bank.Bank("KPYL")
 
     def main(self):
         print("^^^^ WELCOME TO LYPL BANK^^^^\n KINDLY SELECT AN OPTION TO BEGIN\n")
         self.showMainMenu()
 
-    def showMainMenu():
+    def showMainMenu(self):
+        while True:
+            print("1. Select Account\n2. Open Account\n3. Exit\n")
+            option = int(input("Please enter the option to continue: "))
+            if option == 1:
+                account_number = int(input("Enter the account number in range (1000,): "))
+                account = self.bank_obj.searchAccount(account_number)
+                if account is not None:
+                    self.showAccountMenu(account)
+                else:
+                    print("Account not found")
+
+
+            elif option == 2:
+                 self.bank_obj.openAccount()
+                        
+            else :
+                print("exiting")
+                break        
         
 
     def showAccountMenu():
