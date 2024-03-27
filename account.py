07c4a9c3a71383e20e322d616ee8f584e1a17c25
Shuhbam Bhardwaj <bhardwa7@sheridancollege.ca>
2024-03-26 21:10:43 -0400

    Created deposit and withdraw method for account class.

--- account.py
+++ account.py
@@ -2,8 +2,8 @@ class Account:
     def __init__(self,accountNumber, accountHolderName, currentBalance):
         self.currentBalance = currentBalance
         self.rateOfInterest = float(2.5)
-        self.accountNumber = accountNumber
-        self.accountHolderName = accountHolderName
+        self.setAccountNumber(accountNumber)
+        self.setAccountHolderName(accountHolderName)
 
 
 
@@ -14,7 +14,7 @@ class Account:
         return self.accountHolderName
     
     def setAccountNumber(self,accountNumber:int):
-        if     1000<= self.accountNumber <= 10000:
+        if     1000<= accountNumber <= 10000:
             raise Exception("Account number should be in the range (1000,10000)")
         self.accountNumber = accountNumber
 
@@ -25,5 +25,18 @@ class Account:
 
     def getcurrentBalance(self):
         return self.currentBalance
+    
+    def depositAmount(self,amount):
+        if amount > 0:
+            self.currentBalance+=amount
+        else:
+            return "Please enter the valid amount"
+        
+    def __str__(self):
+        return f"\n****Account Updated**** \n Name: {self.getAccountHolderName()}\n|\n Account Number: {self.getAccountNumber()}\n|\n Balance: ${self.currentBalance}\n"
+        
+    
+    def withdraw(self):
+        raise NotImplementedError("Defined in saving and chequing class")
     
 
