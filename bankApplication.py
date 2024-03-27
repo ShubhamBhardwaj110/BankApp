0032a30f323dfd4845c03a2c18ceccf13ae0a0ae
Shuhbam Bhardwaj <bhardwa7@sheridancollege.ca>
2024-03-26 21:53:08 -0400

    In bankApplication class/module show account menu method is created. In this method a loop is created which will ask the user to look for services in account menu such as: check balance,deposit,withdraw etc.

--- bankApplication.py
+++ bankApplication.py
@@ -12,7 +12,7 @@ class bankApplication:
             print("1. Select Account\n2. Open Account\n3. Exit\n")
             option = int(input("Please enter the option to continue: "))
             if option == 1:
-                account_number = int(input("Enter the account number in range (1000,): "))
+                account_number = int(input("Enter the account number in range (1000,1000000): "))
                 account = self.bank_obj.searchAccount(account_number)
                 if account is not None:
                     self.showAccountMenu(account)
@@ -28,5 +28,32 @@ class bankApplication:
                 break        
         
 
-    def showAccountMenu():
-        pass
+    def showAccountMenu(self,account):
+        while True:
+            print("1.Check Balance\n2.Deposit\n3.Withdraw\n4.View Transactions\n5.Exit")
+            option = int(input("Please select an option to continue: "))
+            if option == 1:
+                print(f"Current balance: ${account.getcurrentBalance()}")
+
+
+            elif option ==2:
+                amount = int(input("Please enter the amount to deposit: "))
+                account.depositAmount(amount)
+                print(f"You have deposited ${amount}")
+                print("Remaining Balance: $",account.getcurrentBalance())
+
+            elif option ==3:
+                amount = int(input("Please enter the amount to withdraw: "))
+                account.withdraw(amount)
+                print(f"You have withdrawn ${amount}")
+                print("Remaining Balance: $",account.getcurrentBalance())
+
+            elif option ==4:
+                pass
+
+            elif option ==5:
+
+                break
+
+appobj = bankApplication()
+appobj.main()
